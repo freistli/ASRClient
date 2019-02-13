de671c9402d2687805b71eaf4c151d7f5f6527a0
Freist Li <fauna179@hotmail.com>
2019-02-13 17:41:44 +0800

    Update ASRClient_py27.py

--- ASRClient_py27.py
+++ ASRClient_py27.py
@@ -2,6 +2,7 @@
 #python 2.7
 #python -m pip install pyaudio
 #python -m pip install --user ws4py==0.3.2
+#python -m pip install requests
 
 import pyaudio
 import wave
@@ -18,6 +19,7 @@ import os
 import subprocess
 import shlex
 import ast
+import requests
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
@@ -146,7 +148,7 @@ class MyClient(WebSocketClient):
 
 def RunWSClient():
     parser = argparse.ArgumentParser(description='Command line client for kaldigstserver')
-    parser.add_argument('-u', '--uri', default="ws://localhost:8888/client/ws/speech", dest="uri", help="Server websocket URI")
+    parser.add_argument('-u', '--uri', default="ws://localhost:8888/", dest="uri", help="Server websocket URI")
     parser.add_argument('-r', '--rate', default=32000, dest="rate", type=int, help="Rate in bytes/sec at which audio should be sent to the server. NB! For raw 16-bit audio it must be 2*samplerate!")
     parser.add_argument('--save-adaptation-state', help="Save adaptation state to file")
     parser.add_argument('--send-adaptation-state', help="Send adaptation state from file")
@@ -157,16 +159,34 @@ def RunWSClient():
     content_type = args.content_type
     if content_type == '' and args.audiofile.name.endswith(".raw"):
         content_type = "audio/x-raw, layout=(string)interleaved, rate=(int)%d, format=(string)S16LE, channels=(int)1" %(args.rate/2)
-    ws = MyClient(args.audiofile, args.uri + '?%s' % (urllib.urlencode([("content-type", content_type)])), byterate=args.rate,
+    ws = MyClient(args.audiofile, args.uri + '/client/ws/speech' + '?%s' % (urllib.urlencode([("content-type", content_type)])), byterate=args.rate,
                   save_adaptation_state_filename=args.save_adaptation_state, send_adaptation_state_filename=args.send_adaptation_state)
     ws.connect()
     result = ws.get_full_hyp()
     print result.encode('utf-8')
 
+def RunHTTPClient():
+    parser = argparse.ArgumentParser(description='Command line client for kaldigstserver')
+    parser.add_argument('-u', '--uri', default="http://localhost:8888/", dest="uri", help="Server websocket URI")
+    parser.add_argument('-r', '--rate', default=32000, dest="rate", type=int, help="Rate in bytes/sec at which audio should be sent to the server. NB! For raw 16-bit audio it must be 2*samplerate!")
+    parser.add_argument('--save-adaptation-state', help="Save adaptation state to file")
+    parser.add_argument('--send-adaptation-state', help="Send adaptation state from file")
+    parser.add_argument('--content-type', default='', help="Use the specified content type (empty by default, for raw files the default is  audio/x-raw, layout=(string)interleaved, rate=(int)<rate>, format=(string)S16LE, channels=(int)1")
+    parser.add_argument('audiofile', help="Audio file to be sent to the server", type=argparse.FileType('rb'), default=sys.stdin)
+    args = parser.parse_args()
+
+    r = requests.put(args.uri + '/client/dynamic/recognize', data=args.audiofile)
+    res = r.content.decode('unicode_escape').encode('utf-8')
+
+    data = json.loads(res)
+    
+    print (res)
+    print (data['hypotheses'][0]['utterance'].encode('utf-8'))
 
 def main():
-    RecordVoice()    
-    RunWSClient()
+    #RecordVoice()    
+    #RunWSClient()
+    RunHTTPClient()
 
 if __name__ == "__main__":
     main()
